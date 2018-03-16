7eb427c311aab6761e240963a9da3107f7881f6a
Toby Chui <tobychui@users.noreply.github.com>
2018-03-16 18:48:50 +0800

    Add files via upload

---
+++ Backup/ScreenShotCapture/ConfirmBox.Designer.vb
@@ -0,0 +1,71 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ConfirmBox
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(ConfirmBox))
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackgroundImage = CType(resources.GetObject("Button1.BackgroundImage"), System.Drawing.Image)
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.Location = New System.Drawing.Point(0, 0)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(67, 32)
+        Me.Button1.TabIndex = 0
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.White
+        Me.Button2.BackgroundImage = CType(resources.GetObject("Button2.BackgroundImage"), System.Drawing.Image)
+        Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button2.Location = New System.Drawing.Point(65, 0)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(68, 32)
+        Me.Button2.TabIndex = 1
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'ConfirmBox
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Fuchsia
+        Me.ClientSize = New System.Drawing.Size(137, 189)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "ConfirmBox"
+        Me.Text = "ConfirmBox"
+        Me.TopMost = True
+        Me.TransparencyKey = System.Drawing.Color.Magenta
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+End Class
---
+++ Backup/ScreenShotCapture/Setting.vb
@@ -0,0 +1,45 @@
+﻿Public Class Setting
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        My.Settings.SaveLocation = TextBox1.Text
+        My.Settings.Save()
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        My.Settings.SaveLocation = TextBox1.Text
+        My.Settings.Save()
+        Me.Close()
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Setting_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        TextBox1.Text = My.Settings.SaveLocation
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        If My.Computer.FileSystem.DirectoryExists(TextBox1.Text) And TextBox1.Text.Substring(TextBox1.Text.Length - 1, 1) = "\" Then
+            Label3.Text = "Directory Exists"
+            Label3.ForeColor = Color.Green
+        ElseIf My.Computer.FileSystem.DirectoryExists(TextBox1.Text) And TextBox1.Text.Substring(TextBox1.Text.Length - 1, 1) <> "\" Then
+            Label3.Text = "Missing the last '\'"
+            Label3.ForeColor = Color.Orange
+        Else
+            Label3.Text = "Invaid Directory Path!"
+            Label3.ForeColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        Dim folderDlg As New FolderBrowserDialog
+        folderDlg.Description = "Select the folder which you want to save your captures in"
+        'folderDlg.RootFolder = Environment.SpecialFolder.MyDocuments
+        folderDlg.ShowNewFolderButton = True
+        If (folderDlg.ShowDialog() = DialogResult.OK) Then
+            TextBox1.Text = folderDlg.SelectedPath & "\"
+            Dim root As Environment.SpecialFolder = folderDlg.RootFolder
+        End If
+    End Sub
+End Class
